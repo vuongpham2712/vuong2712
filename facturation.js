5e3bb50916e41e6895850f2a2c366f998d241d2e
vuongpham2712 <37682261+vuongpham2712@users.noreply.github.com>
2018-04-02 16:53:56 +0700

    Add files via upload

--- facturation.js
+++ facturation.js
@@ -1,16 +1,19 @@
 var app = angular.module('facturationApp', []);
 app.controller('facturationCtrl', function($scope){
-	$scope.modes = [
-		{model : "Ford Mustang", color : "red"},
-        {model : "Fiat 500", color : "white"},
-        {model : "Volvo XC90", color : "black"}
-	];
-	$scope.dates = ["jours","mois","années"];
-	$scope.datesDefault = $scope.dates[0];
+	$scope.modes = {
+		card: "1",
+		coin: "2"
+	}
+	$scope.dates1 = ["jours","mois","années"];
+	$scope.dates2 = ["jours","mois","années"];
+	$scope.datesDefault = $scope.dates1[0];
+	$scope.ratios = ["%", "@", "$"];
+	$scope.ratiosDefault = $scope.ratios[0];
 	$scope.count = true;
 	$scope.myFun = function(){
 		$scope.count = false;
 	}
+	$scope.ngayd = new Date();
 	console.log("=======================");
 	console.log("=======================");
 });
